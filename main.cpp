1f176d0ad14196578a53b1ecd1731a155d1dc6d0
WizardDude4488 <samuelhitt48@gmail.com>
2026-06-16 15:36:10 -0400

    Worked on maze generator.

--- main.cpp
+++ main.cpp
@@ -28,12 +28,12 @@ int main() {
     }
     //generate the map for the game
     //number code for square types: 0 = tunnel (can walk), 1 = wall (can't walk), 2 = exit (ends game if reached), 3 = skeleton, 4 = bomb, 5 = coin
-    //number code for maze generation tile types: 0 = empty, 1 = generated
-    constexpr int size = 15;
-    array<array<int, size>, size> tile_map{};
-    for (int i = 0; i < size; i++)
+    //number code for maze generation tile types: 0 = empty, 1 = up, 2 = right, 3 = down, 4 = left. If not 0, then not empty.
+    constexpr int mapSize = 15;
+    array<array<int, mapSize>, mapSize> tile_map{};
+    for (int i = 0; i < mapSize; i++)
     {
-        for (int j = 0; j < size; j++)
+        for (int j = 0; j < mapSize; j++)
         {
             tile_map[i][j] = 0;
         }
@@ -41,34 +41,105 @@ int main() {
 
     int number_of_tiles = 15 * 15;
     uniform_int_distribution<int> dist0_15(0, 15);
-
+    /*
     //generate x and y for the exit
     int exitX = dist0_15(gen);
     int exitY = dist0_15(gen);
     tile_map[exitX][exitY] = 2;
-
-    //list of visited tiles
-    //visited[0][0] returns the x coordinate of the zeroeth tile visited
-    vector<array<int, 2>> visited;
-    visited[0] = {exitX, exitY};
-
+    */
     //vector to store the data about adjacent tiles
     vector<array<int, 2>> available;
 
+    //stack and available choice
+    int stackPos = 0;
+    vector<array<int, 2>> stack;
+    stack.push_back({0,0});
+    int indice = 0;
+
+    //current position components
+    int current_x = 0;
+    int current_y = 0;
+
+    //new position components
+    int new_x = 0;
+    int new_y = 0;
+
     //generate a maze
-    while (size(visited) <= number_of_tiles)
+    //less than since condition will be true for the last iteration where size(stack) becomes = number_of_tiles during the iteration, thus completing the tile map
+    while (size(stack) < number_of_tiles)
     {
-        //start at previous location (exit for first iteration)
         //pick an adjacent tile that hasn't been visited
         //if there isn't an unvisited tile adjacent, move backwards through stack by 1 entry
         //move in that direction, set previous tile to direction traveled to reach current tile
         //add tile coordinates to stack
         
-        //check adjacent squares
+        //check if the four cardinal directions are possible
+        //check the possible cardinally adjacent tiles are empty
+        if (current_y - 1 >= 0)
+        {
+            if (tile_map[current_x][current_y - 1] == 0)
+            {
+              available.push_back({current_x, current_y - 1});
+            }
+        }
+        if (current_x + 1 < mapSize)
+        {
+            if (tile_map[current_x + 1][current_y] == 0)
+            {
+                available.push_back({current_x + 1, current_y});
+            }
+        }
+        if (current_y + 1 < mapSize)
+        {
+            if (tile_map[current_x][current_y + 1] == 0)
+            {
+                available.push_back({current_x, current_y + 1});
+            }
+        }
+        if (current_x - 1 >= 0)
+        {
+            if (tile_map[current_x - 1][current_y] == 0)
+            {
+                available.push_back({current_x - 1, current_y});
+            }
+        } else
+        {
+            //set the available vector to empty if no cardinally adjacent tiles are available
+            available.clear();
+        }
 
+        if (size(available) == 0)
+        {
+            //move backwards in the stack of visited tiles by one if the available vector is empty
+            stackPos -= 1;
+            current_x = stack[stackPos][0];
+            current_y = stack[stackPos][1];
+        } else if (size(available) != 0)
+        {
+            //create a new random number distribution based on the size of the available vector
+            uniform_int_distribution<int> distAvailable(0, size(available) - 1);
+            indice = distAvailable(gen);
+
+            new_x = available[indice][0];
+            new_y = available[indice][1];
+
+            //set direction for current tile based on location of new tile
+            if (new_x - current_x > 0) tile_map[current_x][current_y] = 2; //right
+            if (new_x - current_x < 0) tile_map[current_x][current_y] = 4; //left
+            if (new_y - current_y > 0) tile_map[current_x][current_y] = 3; //down
+            if (new_y - current_y < 0) tile_map[current_x][current_y] = 1; //up
+
+            //after direction is written to the tile map at current position, add current position to the stack and update current position
+            stack.push_back({current_x, current_y});
+
+            current_x = new_x;
+            current_y = new_y;
+        }
+        //clear available vector before use in next iteration
+        available.clear();
 
     }
-
+/*
     //transform the tiles into map coordinates
     while (generated < tiles)
     {
@@ -84,7 +155,7 @@ int main() {
     //then randomly select numbers within the range of the number of free spaces
     //after each number is generated, place the appropriate item there and remove it from the list
 
-    /*place money
+     place money
     int money = 0;
     while (money < 50)
     {
@@ -92,11 +163,7 @@ int main() {
         int moneyY = dist0_99(gen);
         if map[]
         money += 1;
-    }*/
+    } */
 
-
-    //while loop for the game
-    while (running)
-    {
-    }
+    return 0;
 };
